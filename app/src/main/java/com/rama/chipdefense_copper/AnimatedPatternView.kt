e38e0741d16fa3058cd8f9a7d613c9f0d017ce5b
Miguel Rivas <github.5yeil@simplelogin.fr>
2026-02-17 07:05:01 -0800

    different patterns per view

--- app/src/main/java/com/rama/chipdefense_copper/AnimatedPatternView.kt
+++ app/src/main/java/com/rama/chipdefense_copper/AnimatedPatternView.kt
@@ -9,7 +9,6 @@ import android.graphics.Paint
 import android.graphics.Shader
 import android.util.AttributeSet
 import android.view.View
-import com.rama.chipdefense_copper.utils.vectorToBitmap
 
 class AnimatedPatternView @JvmOverloads constructor(
     context: Context,
@@ -17,29 +16,41 @@ class AnimatedPatternView @JvmOverloads constructor(
 ) : View(context, attrs) {
 
     private val paint = Paint()
-    private val shader: BitmapShader
     private val matrix = Matrix()
-
+    private var shader: BitmapShader? = null
     private var offset = 0f
 
     init {
-        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.background_pattern)
+        // default pattern
+        var patternResId = R.drawable.background_pattern_night
 
-        shader = BitmapShader(
-                bitmap,
-                Shader.TileMode.REPEAT,
-                Shader.TileMode.REPEAT
-        )
+        // read XML attribute
+        attrs?.let {
+            val typedArray = context.obtainStyledAttributes(it, R.styleable.AnimatedPatternView)
+            val mode = typedArray.getString(R.styleable.AnimatedPatternView_mode)
+            if (!mode.isNullOrEmpty()) {
+                // dynamically get drawable id from name
+                val resId = resources.getIdentifier(
+                        "background_pattern_$mode",
+                        "drawable",
+                        context.packageName
+                )
+                if (resId != 0) {
+                    patternResId = resId
+                }
+            }
+            typedArray.recycle()
+        }
 
+        val bitmap = BitmapFactory.decodeResource(resources, patternResId)
+        shader = BitmapShader(bitmap, Shader.TileMode.REPEAT, Shader.TileMode.REPEAT)
         paint.shader = shader
     }
 
     override fun onDraw(canvas: Canvas) {
-        offset += 0.5f          // speed
-        matrix.setTranslate(offset, offset)  // diagonal
-        shader.setLocalMatrix(matrix)
-
+        offset += 0.5f
+        shader?.setLocalMatrix(matrix.apply { setTranslate(offset, offset) })
         canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paint)
-        invalidate() // keep animating
+        invalidate()
     }
 }
